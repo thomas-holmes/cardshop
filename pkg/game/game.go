47b7914e3fc8692ae7289fa9a79278dde5e564c5
Thomas Holmes <shadox@gmail.com>
2018-06-12 21:34:03 -0400

    Basic render loop

--- pkg/game/game.go
+++ pkg/game/game.go
@@ -2,7 +2,8 @@ package game
 
 import (
 	"fmt"
-	"time"
+	"log"
+	"runtime"
 
 	"github.com/veandco/go-sdl2/sdl"
 )
@@ -11,14 +12,49 @@ import (
 func Run() error {
 	fmt.Println("In game.Run()")
 
-	window, err := sdl.CreateWindow("cardshop", 0, 0, 800, 600, 0)
+	runtime.LockOSThread()
+
+	window, err := sdl.CreateWindow("cardshop", 100, 100, 800, 600, sdl.WINDOW_RESIZABLE)
 	if err != nil {
 		return err
 	}
 
-	time.Sleep(2 * time.Second)
+	renderer, err := sdl.CreateRenderer(window, 0, sdl.RENDERER_ACCELERATED|sdl.RENDERER_PRESENTVSYNC)
+	if err != nil {
+		return err
+	}
 
-	_ = window
+	_ = renderer
+
+	for !quit {
+		for e := sdl.PollEvent(); e != nil; e = sdl.PollEvent() {
+			processEvent(e)
+		}
+
+		if err := renderer.Clear(); err != nil {
+			panic(err)
+		}
+		renderer.SetDrawColor(127, 127, 255, 0)
+		renderer.Present()
+	}
+
+	sdl.Quit()
 
 	return nil
+}
+
+var quit bool
+
+func processEvent(event sdl.Event) {
+	switch e := event.(type) {
+	case *sdl.KeyboardEvent:
+		log.Println(e)
+		if e.Keysym.Sym == sdl.K_ESCAPE {
+			quit = true
+		}
+	case *sdl.QuitEvent:
+		quit = true
+	default:
+		//
+	}
 }
